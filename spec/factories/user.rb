ecab2def1e0d6d899eb890eeb7463352c4ef9a8d
yusuketakamatsu <sleepingforest03@gmail.com>
2019-10-14 22:54:49 +0900

    add user information test.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,4 +3,8 @@ FactoryBot.define do
     name { 'tarou' }
     email { 'exapmle@gmail.com' }
   end
+
+  factory :user_information do
+    detail { 'detail' }
+  end
 end
--- spec/requests/graphql/query/user_spec.rb
+++ spec/requests/graphql/query/user_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'user query', type: :request do
   subject { post graphql_path, params: { query: query } }
 
   let!(:user) { FactoryBot.create(:user) }
+  let!(:user_information) {
+    FactoryBot.create(:user_information, user_id: user.id)
+  }
 
   let(:query) do
     <<~QUERY
@@ -12,6 +15,9 @@ RSpec.describe 'user query', type: :request do
           id
           email
           name
+          user_information {
+            detail
+          }
         }
       }
     QUERY
@@ -23,5 +29,6 @@ RSpec.describe 'user query', type: :request do
     expect((json[:data][:user][:id]).to_i).to eq user.id
     expect(json[:data][:user][:email]).to eq user.email
     expect(json[:data][:user][:name]).to eq user.name
+    expect(json[:data][:user][:user_information][:detail]).to eq user_information.detail
   end
 end
