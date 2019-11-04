62705ce38953c126ef712cc55f4b9ad59831954c
yusuketakamatsu <sleepingforest03@gmail.com>
2019-11-04 16:05:25 +0900

    recover without connection.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,5 +9,10 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    field :user, Types::UserType, null: false
+    def user
+      context[:current_user]
+    end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,10 +4,10 @@ module Types
     field :name, String, null: false
     field :email, String, null: false
 
-    #field :test, String, null: false # method
-    #field :posts_in_date, [Types::PostType], null: false
+    field :test, String, null: false # method
+    field :posts_in_date, [Types::PostType], null: false
 
-    #field :user_information, Types::UserInformationType, null: false
+    field :user_information, Types::UserInformationType, null: false
     #connection :posts, Types::PostType.connection_type, null: false
   end
 end
